4215890a1e7209ee20056fd63c6d58e9461cf223
Hugo Kawamata <hugokawamata@gmail.com>
2015-12-30 16:01:02 +1000

    Final Website
    Add js and better css

--- js/site.js
+++ js/site.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
-
+console.log("bb")
 //--------------Mobile Orientation--------------//
+	mobile_resize();
 	$(window).resize(function(){
-		if ($window).width() < 768){
-			$
-	}
-	})
+		mobile_resize();
+	});
 
+
+function mobile_resize(){
+	if ($(window).width() < 660 || $(window).width() < $(window).height()){
+		$("header").width("90%");
+		$("#main_body").width("50%");
+		console.log("mobile")
+	}
+	else{
+		$("header").width("45%");
+		$("#main_body").width("40%");
+		console.log("dekstop")
+	};
+	
+};
 
 
 
