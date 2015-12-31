cd1759b18a3afa00b2a84321022bdd344c241260
Hugo Kawamata <hugokawamata@gmail.com>
2015-12-31 17:15:54 +1000

    Phone Test
    Add broken css for test

--- js/site.js
+++ js/site.js
@@ -10,8 +10,9 @@ console.log("bb")
 function mobile_resize(){
 	if ($(window).width() < 660 || $(window).width() < $(window).height()){
 		$("header").width("90%");
+		$("header").height($(window).height())
 		$("#main_body").width("50%");
-		console.log("mobile")
+		
 	}
 	else{
 		$("header").width("45%");
